16d8b014bdae4682f8196e5d8889723fe2736542
roy1111 <30957505+roy1111@users.noreply.github.com>
2017-08-29 00:36:23 +0300

    Update InsFllow.py

--- InsFllow.py
+++ InsFllow.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3.5
+#!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 import time
@@ -18,7 +18,7 @@ from selenium.webdriver.chrome.options import Options
 def loginToAccount(UsrName, Password):
     ## GOES TO INSTAGRAM LOGIN PAGE
     driver.get('https://www.instagram.com/accounts/login/')
-    print (driver.title).encode('utf-8')
+    print driver.title.encode('utf-8')
 
     ## ENTERS THE USERNAME AND PASSWORD
     user = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, "//input[@name='username']")))
@@ -29,7 +29,7 @@ def loginToAccount(UsrName, Password):
     time.sleep(1)
     driver.find_element_by_xpath("//button[contains(.,'Log in')]").click()
     time.sleep(2)
-    print (driver.title).encode('utf-8')
+    print driver.title.encode('utf-8')
 
 
 def enterCelebrityAccountFollowers(url):
@@ -37,16 +37,18 @@ def enterCelebrityAccountFollowers(url):
     driver.get(url)
 
     ## ENTERS CELEBRITY ACCOUNT FOLLOWERS
-    Followers_button = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, 'followers')))
+    Followers_button = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, 'followers')))
     Followers_button.click()
-    print (driver.title).encode('utf-8')
+    print driver.title.encode('utf-8')
 
 
 def getInsideSomeAccount(index):
     ## ENTERS THE ACCOUNT PROFILE AND WAITS FOR ALL PROFILES TO LOAD - THEN CLICKS ON PROFILE BY INDEX
-    WebDriverWait(driver, 5).until(EC.presence_of_all_elements_located((By.CLASS_NAME, '_6e4x5')))  ##ELEMENT NEEDS CHANGE* FINDS ALL LIST ITEMS
-    Profile = WebDriverWait(driver, 5) \
-        .until(EC.presence_of_all_elements_located((By.XPATH, "//*[@class='_2g7d5 notranslate _o5iw8']")))[
+    WebDriverWait(driver, 5).until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME, '_6e4x5')))  ##ELEMENT NEEDS CHANGE* FINDS ALL LIST ITEMS
+    Profile = WebDriverWait(driver, 5).until(
+        EC.presence_of_all_elements_located((By.XPATH, "//*[@class='_2g7d5 notranslate _o5iw8']")))[
         index]  ## ELEMENT NEEDS CHANGE
 
     Profile.click()
@@ -67,75 +69,127 @@ def followActiveAccount():
     AmountOfActiveFollowed = 0
     AmountOfFectiveFollowed = 0
     index = 0
+    FollowedUrList = []
+
+    enterCelebrityAccountFollowers(celebrityAccountURL)
+    getInsideSomeAccount(index)
+
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located(
+        (By.CLASS_NAME, '_fd86t')))  ##ELEMENT NEEDS CHANGE* WAITS UNTIL THE AMOUNT POSTS ELEMENT IS AVAILABLE
+
+    print "Site At Profile: ", driver.title.encode('utf-8')
+    time.sleep(2)
+
+    for y in range(0, 12):
+        for x in range(0, 80):  ## it does one more
+
+            follow_button = driver.find_element_by_xpath("//button[contains(.,'Follow')]")  ## NO NEED TO CHANGE ELEMENT
+            FollowedUrList.append(driver.current_url)
+
+            follow_button.click()
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+                (By.XPATH, "//button[contains(.,'Following') or contains(.,'Requested')]")))
+            now = time.time()
+
+            enterCelebrityAccountFollowers(celebrityAccountURL)
+            index = 0
+
+            while True:
+
+                try:
+                    getInsideSomeAccount(index)
+                    time.sleep(2)
+                    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME,
+                                                                                   '_fd86t')))  ##ELEMENT NEEDS CHANGE* WAITS UNTIL THE AMOUNT POSTS ELEMENT IS AVAILABLE
+                    print "Site At Profile: ", driver.title.encode('utf-8')
+
+                    PostAmount = driver.find_element_by_class_name('_fd86t').text
+                    print 'Number Of Posts: ', PostAmount
+
+                    follow_button1 = driver.find_elements_by_xpath(
+                        "//button[contains(.,'Following')]")  ## NO NEED TO CHANGE ELEMENT
+                    follow_button2 = driver.find_elements_by_xpath(
+                        "//button[contains(.,'Requested')]")  ## NO NEED TO CHANGE ELEMENT
+
+                    ## CHECKS IF ACCOUNT HAS NOT ALREADY BEEN FOLLOWED
+                    if follow_button1.__len__() == 0 and follow_button2.__len__() == 0:
+
+                        after = time.time()
+
+                        if int(after) - int(now) > 44:  ##THERE IS A TIME.SLEEP FOR 2 SEC
+                            AmountOfFectiveFollowed += 1
+                            print 'Fictive Follow: ', AmountOfFectiveFollowed
+                            break
+
+                        ## CHECKS IF ACCOUNT HAS MORE THAN 40 POSTS - IF DOES, FOLLOWED
+                        elif 40 <= int(PostAmount) < 151:
+                            print 'Active Follow: ', AmountOfActiveFollowed
+                            after = time.time()
+                            LoadingTime = waitUntilTimeReached(now, after, 44)
+                            time.sleep(LoadingTime)
+                            AmountOfActiveFollowed += 1
+                            break
+
+                    ## IF, ONE OF THE IF'S STATEMENTS ARE FALSE, DRIVER GOES BACK TO LIST TO TRY NEXT ACCOUNT
+                    index += 1
+
+                    ## AFTER 20 PROFILES, LIST INDEX WILL BE OUT OF RANGE, SO THIS WILL HANDLE
+                    if index > 19:
+                        index = 0
+                        enterCelebrityAccountFollowers(celebrityAccountURL)
+
+                    else:
+                        driver.back()
+
+                except Exception as e:
+                    print e
+                    driver.back()
+                    enterCelebrityAccountFollowers(celebrityAccountURL)
+
+    print "TODAY PROGRAM FOLLOWED: ", FollowedUrList.__len__()
+    return FollowedUrList
+
+
+def Unfollow(FollowedUrList):
+    Unfollowed = 0
+
+    print FollowedUrList
     now = time.time()
 
-    while True:
-        getInsideSomeAccount(index)
-        time.sleep(3)
-        print ("Site At Profile: ", driver.title.encode('utf-8'))
-
+    for url in FollowedUrList:
         try:
-            WebDriverWait(driver, 2).until(
-                EC.presence_of_element_located(
-                    (By.CLASS_NAME, '_fd86t')))  ##ELEMENT NEEDS CHANGE* WAITS UNTIL THE ELEMENT IS AVAILABLE
-            PostAmount = driver.find_element_by_class_name('_fd86t').text
-            print ('Number Of Posts: ', PostAmount)
+            driver.get(url)
+            Unfollow_button = WebDriverWait(driver, 2).until(EC.presence_of_element_located(
+                    (By.XPATH, "//button[contains(.,'Following') or contains(.,'Requested')]")))
 
-            follow_button1 = driver.find_elements_by_xpath(
-                "//button[contains(.,'Following')]")  ## NO NEED TO CHANGE ELEMENT
-            follow_button2 = driver.find_elements_by_xpath(
-                "//button[contains(.,'Requested')]")  ## NO NEED TO CHANGE ELEMENT
+            after = time.time()
 
-            ## CHECKS IF ACCOUNT HAS NOT ALREADY BEEN FOLLOWED
-            if follow_button1.__len__() == 0 and follow_button2.__len__() == 0:
+            LoadingTime = waitUntilTimeReached(now, after, 45)
+            time.sleep(LoadingTime)
 
-                after = time.time()
+            Unfollow_button.click()
 
-                if int(after) - int(now) > 91:
-                    follow_button = driver.find_element_by_xpath(
-                        "//button[contains(.,'Follow')]")  ## NO NEED TO CHANGE ELEMENT
-                    follow_button.click()
-                    WebDriverWait(driver, 10).until(
-                        EC.presence_of_element_located(
-                            (By.XPATH, "//button[contains(.,'Following') or contains(.,'Requested')]")))
-                    AmountOfFectiveFollowed += 1
-                    print ('Fictive Follow: ', AmountOfFectiveFollowed)
-                    return AmountOfFectiveFollowed
+            WebDriverWait(driver, 5).until(EC.presence_of_element_located(
+                (By.XPATH, "//button[contains(.,'Follow')]")))
 
-                ## CHECKS IF ACCOUNT HAS MORE THAN 40 POSTS - IF DOES, FOLLOWED
-                elif 40 <= int(PostAmount) < 151:
-                    follow_button = driver.find_element_by_xpath(
-                        "//button[contains(.,'Follow')]")  ## NO NEED TO CHANGE ELEMENT
-                    follow_button.click()
-                    WebDriverWait(driver, 10).until(
-                        EC.presence_of_element_located(
-                            (By.XPATH, "//button[contains(.,'Following') or contains(.,'Requested')]")))
-                    AmountOfActiveFollowed += 1
-                    print ('Active Follow: ', AmountOfActiveFollowed)
-                    after = time.time()
-                    LoadingTime = waitUntilTimeReached(now, after, 91)
-                    time.sleep(LoadingTime)
-                    return AmountOfActiveFollowed
+            Unfollowed += 1
+            print 'Unfollowed ', Unfollowed, 'accounts'
 
-            ## IF, ONE OF THE IF'S STATEMENTS ARE FALSE, DRIVER GOES BACK TO LIST TO TRY NEXT ACCOUNT
-            index += 1
+            now = time.time()
 
-            ## AFTER 20 PROFILES, LIST INDEX WILL BE OUT OF RANGE, SO THIS WILL HANDLE
-            if index > 19:
-                index = 0
-                enterCelebrityAccountFollowers(celebrityAccountURL)
+        except Exception as e:
+            print e
+            pass
 
-            else:
-                driver.back()
-
-        except Exception as e:  ## THE TRY AND CATCH WERE MADE FOR CONDITION TO KNOW THE PICTURE DATE (IF IT HAD BEEN UPLOADED LATELY)
-            print (e)
-            return 0
+    print 'UNFOLLOWED ACCOUNTS FOR TODAY:', Unfollowed
 
 
+username = 'onpoint_facts'
+password = '158158123123'
+celebrityAccountURL = 'https://www.instagram.com/luissuarez9/'
 
 GOOGLE_CHROME_BIN = r"/app/.apt/usr/bin/google-chrome"
-CHROMEDRIVER_PATH  = r"/app/.chromedriver/bin/chromedriver"
+CHROMEDRIVER_PATH = r"/app/.chromedriver/bin/chromedriver"
 
 chrome_options = Options()
 chrome_options.binary_location = GOOGLE_CHROME_BIN
@@ -144,24 +198,9 @@ chrome_options.add_argument('--no-sandbox')
 driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
 
 driver.maximize_window()
-        
-username = 'onpoint_facts'
-password = '158158123123'
-celebrityAccountURL = 'https://www.instagram.com/luissuarez9/'
-
-SumFollowesToday = 0
-CounterUntilOneDay = 0
 
 loginToAccount(username, password)
 
 while True:
-    for x in range(0, 40):
-        enterCelebrityAccountFollowers(celebrityAccountURL)
-        SumFollowesToday += followActiveAccount()
-
-    ## CHECKS IF 24 HOURS HAD PASSED - SUPPOSED TO PRINT 960 FOLLOWERS
-    CounterUntilOneDay += 1
-    if CounterUntilOneDay == 24:
-        print ('NUMBER OF FOLLOWES DONE TODAY: ', SumFollowesToday)
-        CounterUntilOneDay = 0
-        SumFollowesToday = 0
+    Followed = followActiveAccount()
+    Unfollow(Followed)
